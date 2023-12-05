ba50404cebdd6db84aa88233a40d2a0fff00321c
Satapas T <satapastanachotpaisit@gmail.com>
2023-12-05 11:45:06 +0000

    implemented part of the web surfer

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ from bs4 import BeautifulSoup
 import re
 
 class scraper:
-    def __init__(self, URL, key_store_input=None, key_counter_input=None):
-        page = requests.get(URL)
-        self.soup = BeautifulSoup(page.content, "html.parser")
-        if key_store_input == None:
-            self.key_word_store = []
-            self.key_word_counter = []
-        else:
-            self.key_word_store = key_store_input
-            self.key_word_counter = key_counter_input
+    def __init__(self, URL, key_store_input=None, key_counter_input=None, URL_holder=None, visited=None):
+        self.page = requests.get(URL)
+        self.soup = BeautifulSoup(self.page.content, "html.parser")
+        self.key_word_store = []
+        self.key_word_counter = []
+        self.visited = [URL]
+        self.URL_holder = []
+    
+    def update_soup(self,URL):
+        self.page = requests.get(URL)
+        self.soup = BeautifulSoup(self.page.content, "html.parser")
 
     def keyword_finder(self):
         span_finder = self.soup.find_all("span")
@@ -33,12 +35,20 @@ class scraper:
                 else:
                     self.key_word_counter[self.key_word_store.index(word_seperator_arr[0])] += 1
                     word_seperator_arr.pop(0)
-
         for i in range(len(self.key_word_store)):
             if self.key_word_store[i].isalpha():
                 print(f"{self.key_word_store[i]} : {self.key_word_counter[i]}")
+        
+    def web_surf(self):
+        web_finder = self.soup.find_all("a")
+        for web_finder in web_finder:
+            link_tag_string = str(web_finder)
+            if "https://jujutsu-kaisen.fandom.com/wiki" in link_tag_string:
+                print(link_tag_string)
+            #index_href = link_tag_string.find('href="') + 6
+            #print(link_tag_string[index_href::])
 
 
 scraper = scraper("https://jujutsu-kaisen.fandom.com/wiki/Jujutsu_Kaisen_Wiki")
-scraper.keyword_finder()
+print(scraper.web_surf())
 #print(soup)
